5f8a72cccdbcce03f05d1a28a17aaa2e766ad340
Santiago Mendoza <srjmendoza@hotmail.com>
2023-06-03 16:18:27 -0300

    Configurando el auricular

--- src/components/Bluetooth/Bluetooth.js
+++ src/components/Bluetooth/Bluetooth.js
@@ -3,24 +3,38 @@ import styles from './Bluetooth.module.css';
 import { BsBluetooth } from "react-icons/bs";
 
 const BluetoothButton = () => {
-  const connectToDevice = async () => {
-    try {
-      const device = await navigator.bluetooth.requestDevice({
-        filters: [{ services: ['battery_service'] }]
-      });
+    const connectToDevice = async () => {
+        try {
+            const device = await navigator.bluetooth.requestDevice({
+                filters: [
+                    { services: ['battery_service'] },
+                    { services: ['headphones_service_uuid'] }
+                ]
+            });
+        console.log(device.name);
 
-      console.log(device.name);
+        const server = await device.gatt.connect();
 
-      const server = await device.gatt.connect();
+        // Obtener el servicio deseado por su UUID
+        const service = await server.getPrimaryService('headphones_service_uuid');
 
-      // Realiza acciones adicionales con el servidor GATT
+        // Obtener la característica deseada por su UUID
+        const characteristic = await service.getCharacteristic('headphones_characteristic_uuid');
+
+        // Leer el valor de la característica
+        const value = await characteristic.readValue();
+
+        // Convertir el valor en una cadena
+        const valueString = new TextDecoder().decode(value);
+
+        console.log(valueString);
 
     } catch (error) {
-      console.error("Error al conectar con el dispositivo Bluetooth:", error);
-    }
-  };
+        console.error("Error al conectar con el dispositivo Bluetooth:", error);
+        }
+    };
 
-  return (
+return (
     <button 
         className={styles.bluebutton} 
         onClick={connectToDevice}>
@@ -28,7 +42,7 @@ const BluetoothButton = () => {
             <BsBluetooth />
         </span>
     </button>
-  );
+    );
 };
 
 export default BluetoothButton;
